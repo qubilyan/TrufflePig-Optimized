7831c1940a15ee0b966afb333c1ae543218f42a3
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-09-18 20:41:04 +0000

    more progress

--- scripts/do_cross_val.py
+++ scripts/do_cross_val.py
@@ -3,4 +3,17 @@ import os
 import gc
 
 import pandas as pd
-from steem 
+from steem import Steem
+
+import trufflepig.model as tpmo
+import trufflepig.preprocessing as tppp
+import trufflepig.bchain.getdata as tpgd
+from trufflepig.bchain.mpsteem import MPSteem
+from trufflepig import config
+
+
+def main():
+
+    format=('%(asctime)s %(processName)s:%(name)s:'
+                  '%(funcName)s:%(lineno)s:%(levelname)s: %(message)s')
+   
