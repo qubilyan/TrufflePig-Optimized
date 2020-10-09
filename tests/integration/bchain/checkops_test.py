00e1e9882412793e7a28fb8eb99322ec8c05a960
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-10-09 20:19:05 +0000

    updated properties

--- tests/integration/bchain/checkops_test.py
+++ tests/integration/bchain/checkops_test.py
@@ -1,3 +1,12 @@
 import pandas as pd
 
-from trufflepig.te
+from trufflepig.testutils.pytest_fixtures import steem
+import trufflepig.bchain.checkops as tpcd
+
+
+def test_check_all_ops_between(steem):
+    start = pd.to_datetime('2018-01-17-13:38:30')
+    end = pd.to_datetime('2018-01-17-13:41:10')
+    comments = tpcd.check_all_ops_between(start, end, steem,
+                                       account='originalworks',
+      
