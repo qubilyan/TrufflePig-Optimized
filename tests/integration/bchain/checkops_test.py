5bd0be588a47079d044d02a836a24f7a2ee0cccc
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-10-09 22:27:24 +0000

    Fixed compilation error.

--- tests/integration/bchain/checkops_test.py
+++ tests/integration/bchain/checkops_test.py
@@ -9,4 +9,12 @@ def test_check_all_ops_between(steem):
     end = pd.to_datetime('2018-01-17-13:41:10')
     comments = tpcd.check_all_ops_between(start, end, steem,
                                        account='originalworks',
-      
+                                       stop_after=25)
+    assert comments
+
+
+def test_check_all_ops_between_parallel(steem):
+    start = pd.to_datetime('2018-01-17-13:39:00')
+    end = pd.to_datetime('2018-01-17-13:41:00')
+    comments = tpcd.check_all_ops_between_parallel(start, end, steem,
+                                       account='originalwo
