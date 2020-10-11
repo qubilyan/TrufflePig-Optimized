482d718b6b5d7460b9779ade9a47d7eee6f5486e
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-10-11 17:48:43 +0000

    oops..

--- tests/integration/bchain/paydelegates_test.py
+++ tests/integration/bchain/paydelegates_test.py
@@ -7,4 +7,12 @@ import trufflepig.bchain.postdata as tpdd
 from trufflepig import config
 
 
-@pytest.mark.skipif(conf
+@pytest.mark.skipif(config.ACTIVE_KEY is None, reason="needs active key")
+def test_pay_delegates(steem):
+
+    tpdd.create_wallet(steem, config.PASSWORD,
+                       config.POSTING_KEY, config.ACTIVE_KEY)
+
+    tppd.pay_delegates(account=config.ACCOUNT,
+                       steem=steem,
+                       current_datetime=pd.datetime.u
