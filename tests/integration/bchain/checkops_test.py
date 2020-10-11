4e38472c42e2bffbeac50427c101a267646e8e3a
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-10-11 13:19:52 +0000

    iommu/tegra: smmu: Fix deadly typo

--- tests/integration/bchain/checkops_test.py
+++ tests/integration/bchain/checkops_test.py
@@ -24,4 +24,8 @@ def test_check_all_ops_between_parallel(steem):
 
 def test_get_parent_posts(steem):
     comments_ap = (('lextenebris' ,'re-smcaterpillar-re-lextenebris-re-smcaterpillar-re-lextenebris-re-boucaron-re-lextenebris-re-boucaron-re-lextenebris-programming-digging-in-the-db-and-hitting-a-wall-20180301t230012700z'),
-   
+                  ('smcaterpillar', 're-trufflepig-re-trufflepig-a-bot-based-on-natural-language-processing-and-machine-learning-to-support-content-curators-and-minnows-20180227t172111-20180227t172607601z'))
+
+    posts = tpcd.get_parent_posts(comments_ap, steem)
+
+    assert len(posts) == 2
---
+++ tests/integration/bchain/getaccountdata_test.py
@@ -0,0 +1,24 @@
+
+import pandas as pd
+
+from trufflepig.testutils.pytest_fixtures import steem
+import trufflepig.bchain.getaccountdata as tpac
+
+
+def test_find_index_offset(steem):
+    now = pd.datetime.utcnow()
+    target = now - pd.Timedelta(days=42)
+    offset, datetime = tpac.find_nearest_index(target, 'cheetah', steem)
+    assert 0 < offset
+    assert abs((target - datetime).seconds) < 3600*48
+
+
+def test_shares_query(steem):
+    result = tpac.get_delegates_and_shares('trufflepig', steem)
+
+    assert 'smcaterpillar' in result
+
+
+def test_payouts(steem):
+    now_24 = pd.datetime.utcnow() + pd.Timedelta(days=1)
+    result, result_steem = tpac.get_delegate_payouts('trufflepig', steem,
