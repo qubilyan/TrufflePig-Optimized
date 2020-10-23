34a157f41f127332d2805f0646639cd399d3ac1a
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-10-23 06:02:01 +0000

    sync with svn

--- tests/integration/pigonduty_test.py
+++ tests/integration/pigonduty_test.py
@@ -1,1 +1,11 @@
-from trufflepig.testutils.pytest
+from trufflepig.testutils.pytest_fixtures import steem
+import trufflepig.pigonduty as tppd
+from tests.integration.model_test import MockPipeline
+from trufflepig import config
+from trufflepig.bchain.poster import Poster
+
+
+def test_call_a_pig(steem):
+    current_datetime = '2018-03-03-18:21:30'
+
+    pipeline = MockPipel
