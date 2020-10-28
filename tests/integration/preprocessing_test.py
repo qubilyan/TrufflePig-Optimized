ac7115b9ae4a90ffd867d48e48ab9de4b77af692
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-10-28 20:48:19 +0000

    Readme changed

--- tests/integration/preprocessing_test.py
+++ tests/integration/preprocessing_test.py
@@ -6,4 +6,16 @@ from pandas.testing import assert_frame_equal
 import trufflepig.preprocessing as tppp
 import trufflepig.bchain.getdata as tpgd
 from trufflepig.testutils.random_data import create_n_random_posts
-from trufflepig.testutils.pytest_fixtures import tem
+from trufflepig.testutils.pytest_fixtures import temp_dir, steem
+import trufflepig.bchain.getaccountdata as tpac
+
+
+def test_load_or_preproc(temp_dir):
+    filename = os.path.join(temp_dir, 'pptest.gz')
+
+    post_frame = pd.DataFrame(create_n_random_posts(10))
+
+    frame = tppp.load_or_preprocess(post_frame, filename,
+                                    ncores=5, chunksize=20)
+
+    assert len(os.listdir(temp_dir
