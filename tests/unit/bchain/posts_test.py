7ad102dd0ea6bf512903aaac9ddf60b2d71f6fc3
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-11-16 16:20:05 +0000

    updated deps.

--- tests/unit/bchain/posts_test.py
+++ tests/unit/bchain/posts_test.py
@@ -3,4 +3,13 @@ import pandas as pd
 import trufflepig.bchain.posts as tbpo
 import trufflepig.filters.textfilters as tptf
 import trufflepig.preprocessing as tppp
-fr
+from trufflepig.testutils import random_data
+
+
+def test_comment():
+    post = random_data.create_n_random_posts(1)[0]
+
+    result = tbpo.truffle_comment(reward=post['reward'],
+                                  votes=post['votes'],
+                                  rank=1,
+                                  topN_link='www.example.com',
