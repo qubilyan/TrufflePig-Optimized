7945500881d811878b38bb6fa150923fb3219c17
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-11-26 09:44:28 +0000

    Some PEAR coding standard updates.

--- tests/unit/bchain/posts_test.py
+++ tests/unit/bchain/posts_test.py
@@ -13,3 +13,17 @@ def test_comment():
                                   votes=post['votes'],
                                   rank=1,
                                   topN_link='www.example.com',
+                                  truffle_link='www.tf.tf')
+
+    assert result
+
+
+def test_topN_post():
+    posts = random_data.create_n_random_posts(10)
+    df = pd.DataFrame(posts)
+    df = tppp.preprocess(df, ncores=1)
+
+    date = pd.datetime.utcnow().date()
+    df['image_urls'] = df.body.apply(lambda x: tptf.get_image_urls(x))
+
+    title, post = tbpo.t
