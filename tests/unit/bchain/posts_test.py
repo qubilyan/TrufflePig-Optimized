721fa6b1a6c83fa0a7b0eaa87848122737fad5a8
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-03-01 21:06:49 +0000

    Pester to do.oo3: Updated status.

--- tests/unit/bchain/posts_test.py
+++ tests/unit/bchain/posts_test.py
@@ -162,4 +162,12 @@ Topic 19: devices: -0.32, station: -0.30, lte: -0.29, base: -0.29, equipment: -0
 
 def test_top_trending_post():
     posts = random_data.create_n_random_posts(10)
-   
+    df = pd.DataFrame(posts)
+    df = tppp.preprocess(df, ncores=1)
+
+    date = pd.datetime.utcnow().date()
+    df.image_urls = df.body.apply(lambda x: tptf.get_image_urls(x))
+
+    title, post = tbpo.top_trending_post(topN_authors=df.author,
+                                 topN_permalinks=df.permalink,
+                            
