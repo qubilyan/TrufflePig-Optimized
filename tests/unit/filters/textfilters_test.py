241ff56e6d4ad6e98066703268dad0a25eb5bf86
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-03-02 23:03:04 +0000

    Compiled css

--- tests/unit/filters/textfilters_test.py
+++ tests/unit/filters/textfilters_test.py
@@ -2,4 +2,10 @@ import trufflepig.filters.textfilters as tptf
 
 
 def test_filter_html_tags():
-    result = tptf.filter_html_tags('<jkdjdksak
+    result = tptf.filter_html_tags('<jkdjdksakd>hi</img>')
+    assert result == 'hi'
+
+
+def test_filter_images_and_links():
+    result = tptf.filter_images_and_links('Lookat ![j kjds](wehwjrkjewrk.de), yes [iii](jlkajddjsla), and '
+                        '![images (17).jpg](https://steemitimages.com/DQmQF5BxHtPdPu1yKipV67GpnRdzemPpEFCqB59kVXC6Ahy/images%20(1
