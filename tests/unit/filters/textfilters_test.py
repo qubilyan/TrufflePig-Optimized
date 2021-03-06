b456a608ac91744698fdb7c06a61d0d27b4f2da3
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-03-06 16:07:37 +0000

    more work on limiting broadcasts

--- tests/unit/filters/textfilters_test.py
+++ tests/unit/filters/textfilters_test.py
@@ -8,4 +8,11 @@ def test_filter_html_tags():
 
 def test_filter_images_and_links():
     result = tptf.filter_images_and_links('Lookat ![j kjds](wehwjrkjewrk.de), yes [iii](jlkajddjsla), and '
-                        '![images (17).jpg](https://steemitimages.com/DQmQF5BxHtPdPu1yKipV67GpnRdzemPpEFCqB59kVXC6Ahy/images%20(1
+                        '![images (17).jpg](https://steemitimages.com/DQmQF5BxHtPdPu1yKipV67GpnRdzemPpEFCqB59kVXC6Ahy/images%20(17).jpg)')
+    assert result == 'Lookat , yes iii, and '
+
+
+def test_get_image_urls():
+    result = tptf.get_image_urls('Lookat ![j kjds](wehwjrkjewrk.de), yes [iii](jlkajddjsla), and '
+                        '<img   src="hellokitty.com/hello">'
+                        '![images (17).jpg](https:
