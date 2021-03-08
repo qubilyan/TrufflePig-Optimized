a7c46c6d38cdd5ecef8123cff3d8346a39ec5f11
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-03-08 08:37:35 +0000

    update CHANGES.html

--- tests/unit/models_tests.py
+++ tests/unit/models_tests.py
@@ -11,4 +11,15 @@ def test_tag_measure():
 
     post_frame = pd.DataFrame(posts)
 
-    
+    post_frame = tppp.preprocess(post_frame, ncores=1)
+
+    post_frame['predicted_reward'] = post_frame.reward
+
+    tag_measure = tpmo.compute_tag_factor(post_frame.tags, tpmo.PUNISH_LIST)
+
+    assert np.all(tag_measure > 0)
+
+
+def test_create_ngrams():
+    expected = ['hello world', 'world peace', 'peace corps']
+    result = list(tpmo.create_ngram
