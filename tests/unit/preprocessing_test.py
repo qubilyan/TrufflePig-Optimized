efb32bf197b802aadadb61427a2ee2c156644b8d
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-03-08 19:31:24 +0000

    Minor config changes

--- tests/unit/preprocessing_test.py
+++ tests/unit/preprocessing_test.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
 import trufflepig.preprocessing as tppp
-from trufflepig.testutils.random_data import create_n
+from trufflepig.testutils.random_data import create_n_random_posts
+from trufflepig.testutils.raw_data import POSTS
+
+
+def test_preprocessing():
+    post_frame = pd.DataFrame(POSTS)
+    filtered = tppp.preprocess(post_frame, ncores=1, min_en_prob=0.5,
+                               max_errors_per_word=0.5,
+                               min_max_num_words=(10, 99999))
+
+    assert len(fi
