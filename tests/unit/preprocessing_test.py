22141b2fe56f46692ee845138b1259601ee4a3e5
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-03-09 15:54:48 +0000

    Update copyright

--- tests/unit/preprocessing_test.py
+++ tests/unit/preprocessing_test.py
@@ -19,4 +19,13 @@ def test_preprocessing_parallel():
     post_frame['permalink'] = ['kkk'+str(irun % 50) for irun in range(100)]
     filtered = tppp.preprocess(post_frame, ncores=5, chunksize=20,
                                min_en_prob=0.5, max_errors_per_word=0.5,
-                       
+                               min_max_num_words=(10, 99999))
+
+    assert len(filtered) > 40
+
+
+def test_preprocessing_random_parallel():
+    posts = create_n_random_posts(50)
+    post_frame = pd.DataFrame(posts)
+    filtered = tppp.preprocess(post_frame, ncores=5, chunksize=10,
+                               min_en_prob=0.5, max_errors_per_w
