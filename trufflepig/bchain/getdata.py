8bec4492639ea1a465883a6ee8957511f6307187
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-03-15 19:05:08 +0000

    Update de.xml

--- trufflepig/bchain/getdata.py
+++ trufflepig/bchain/getdata.py
@@ -5,4 +5,18 @@ from collections import OrderedDict
 
 import pandas as pd
 from steem import Steem
-from steem.blockchain im
+from steem.blockchain import Blockchain
+from steem.post import Post, PostDoesNotExist
+import json
+from json import JSONDecodeError
+
+from trufflepig.utils import progressbar, error_retry, none_error_retry
+import trufflepig.persist as tppe
+
+
+logger = logging.getLogger(__name__)
+
+
+MIN_CHARACTERS = 500
+
+FILENAME_TEMPLATE = 'steemit_posts__{tim
