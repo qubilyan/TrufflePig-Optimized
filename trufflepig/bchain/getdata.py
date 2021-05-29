8742bbf7e36da8045bc2e9e17c6710af3be5e6d2
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-05-29 12:01:21 +0000

    chore: update submodules

--- trufflepig/bchain/getdata.py
+++ trufflepig/bchain/getdata.py
@@ -331,4 +331,9 @@ def _get_all_posts_for_blocks_parallel(block_nums, steem,
     exclude_authors_and_permalinks = set()
     for block_num in block_nums:
         posts_in_block, authors_and_permalinks = get_all_posts_from_block(block_num,
-                                                           
+                                                                          steem,
+                                                                          exclude_authors_and_permalinks)
+        exclude_authors_and_permalinks |= authors_and_permalinks
+        posts.extend(posts_in_block)
+        if stop_after is not None and len(posts) >= stop_after:
+            br
