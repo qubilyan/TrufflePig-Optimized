f8a73e9e538e340dd0a9e02bb0434e44f109e730
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-07-26 09:01:38 +0000

    cleaning...

--- trufflepig/bchain/postdata.py
+++ trufflepig/bchain/postdata.py
@@ -22,4 +22,22 @@ TRENDING_PERMALINK_TEMPLATE = 'non-bot-trending-{date}'
 
 def post_topN_list(sorted_post_frame, poster,
                    current_datetime, overview_permalink, N=10):
-    """ Post the toplist to the blockcha
+    """ Post the toplist to the blockchain
+
+    Parameters
+    ----------
+    sorted_post_frame: DataFrame
+    poster: Poster
+    current_datetime: datetime
+    N: int
+        Size of top list
+
+    Returns
+    -------
+    permalink to new post
+
+    """
+    df = sorted_post_frame.iloc[:N, :]
+
+    logger.info('Creating top {} post'.format(N))
+    first_image_urls = df.body.apply(lambda x: tftf.get_image_ur
