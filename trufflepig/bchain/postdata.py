88b689530d08f3a07db1cc0cae22e4e18b7e1ebe
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-07-26 08:46:12 +0000

    removed debugger

--- trufflepig/bchain/postdata.py
+++ trufflepig/bchain/postdata.py
@@ -8,4 +8,18 @@ from steem.converter import Converter
 import trufflepig.bchain.posts as tfbp
 import trufflepig.bchain.getdata as tfgd
 import trufflepig.filters.textfilters as tftf
-from trufflepig.utils import
+from trufflepig.utils import error_retry
+from trufflepig.bchain.poster import Poster
+
+
+logger = logging.getLogger(__name__)
+
+
+PERMALINK_TEMPLATE = 'daily-truffle-picks-{date}'
+
+TRENDING_PERMALINK_TEMPLATE = 'non-bot-trending-{date}'
+
+
+def post_topN_list(sorted_post_frame, poster,
+                   current_datetime, overview_permalink, N=10):
+    """ Post the toplist to the blockcha
