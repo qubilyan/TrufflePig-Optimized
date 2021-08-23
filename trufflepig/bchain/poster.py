5d8eb94a2b1cd3291bd5ca840204d4fab5fe6bcf
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-08-23 10:25:58 +0000

    Update archive

--- trufflepig/bchain/poster.py
+++ trufflepig/bchain/poster.py
@@ -14,4 +14,10 @@ THRESHOLD = 'threshold'
 
 class Poster(object):
     """A class to allow for posting and taking care of posting intervals"""
- 
+    def __init__(self, steem, account, self_vote_limit=94, waiting_time=WAIT,
+                 no_posting_key_mode=False):
+        self.no_posting_key_mode = no_posting_key_mode
+        self.waiting_time = waiting_time
+        self.last_post_time = time.time() - self.waiting_time
+        self.steem = steem
+        self.accoun
