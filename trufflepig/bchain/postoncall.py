b2e2e5632750ab78024531369fbf749aea4c162e
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-08-30 12:48:03 +0000

    chore: bump version

--- trufflepig/bchain/postoncall.py
+++ trufflepig/bchain/postoncall.py
@@ -31,3 +31,48 @@ def post_on_call(post_frame, topN_link,
     post_frame: DataFrame
     poster: Poster
     topN_link: str
+    filter_voters: set of str
+
+    """
+    weight = min(75 / len(post_frame), 10)
+    truffle_link = 'https://steemit.com/steemit/@{}/{}'.format(poster.account,
+                                                               overview_permalink)
+
+    for kdx, (_, row) in enumerate(post_frame.iterrows()):
+        try:
+            comment = Post('@{}/{}'.format(row.comment_author,
+                                           row.comment_permalink), poster.steem)
+
+            if not tftf.voted_by(row.active_votes, {poster.account}):
+                if row.passed and not tftf.voted_by(row.active_votes, filter_voters):
+
+                        logger.info('Voting and commenting on https://steemit.com/@{author}/{permalink}'
+                                        ''.format(author=row.author, permalink=row.permalink))
+                        reply = tfbp.on_call_comment(reward=row.predicted_reward,
+                                                     author=row.comment_author,
+                                                     votes=row.predicted_votes,
+                                                     topN_link=topN_link,
+                                                     truffle_link=truffle_link)
+
+                        poster.vote(row.author, row.permalink, weight)
+                else:
+                    reply = YOU_DID_NOT_MAKE_IT
+            else:
+                reply = I_WAS_HERE
+
+            replies = comment.steemd.get_content_replies(row.comment_author,
+                                                         row.comment_permalink)
+            reply_authors = set(x['author'] for x in replies)
+            if poster.account not in reply_authors:
+                poster.reply(body=reply,
+                             parent_author=row.comment_author,
+                             parent_permalink=row.comment_permalink)
+            else:
+                logger.info('Already answered {} by {}, will '
+                            'skip!'.format(row.comment_author,
+                                           row.comment_permalink))
+
+        except Exception as e:
+            logger.exception('Something went wrong with row {}.'
+                             'Reconnecting...'.format(row))
+            poster.steem.reconnect()
---
+++ trufflepig/bchain/posts.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+import trufflepig.filters.textfilters as tftf
+
+
+TRUFFLE_IMAGE_SMALL = '![trufflepig](https://raw.githubusercontent.com/SmokinCaterpillar/TrufflePig/master/img/trufflepig17_small.png)'
+TRUFFLE_IMAGE = '![trufflepig](https://raw.githubusercontent.com/SmokinCaterpillar/Truffle
