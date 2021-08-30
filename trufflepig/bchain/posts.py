7aade43ba378667bbf5f65b028edbd0a7eaee03d
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-08-30 18:28:24 +0000

    bcmdhd: Add checks for stack buffer overflows

--- trufflepig/bchain/posts.py
+++ trufflepig/bchain/posts.py
@@ -8,4 +8,11 @@ TRUFFLE_IMAGE = '![trufflepig](https://raw.githubusercontent.com/SmokinCaterpill
 DELEGATION_LINK = 'https://v2.steemconnect.com/sign/delegateVestingShares?delegator=&delegatee=trufflepig&vesting_shares={shares}%20VESTS'
 QUOTE_MAX_LENGTH = 496
 TAGS = ['steemit', 'curation', 'minnowsupport', 'technology', 'community']
-TRENDING_TAGS = ['steemit', 'curation', 'bots', 'technology', 'comm
+TRENDING_TAGS = ['steemit', 'curation', 'bots', 'technology', 'community']
+
+BODY_PREFIX = ''  # to announce tests etc.
+
+
+def truffle_comment(reward, votes, rank, topN_link, truffle_link, truffle_image_small=TRUFFLE_IMAGE_SMALL):
+    """Creates a comment made under an upvoted toplist post"""
+    post = """**Congratulations!** Your post has been selected as a daily Steemit truffle! It is list
