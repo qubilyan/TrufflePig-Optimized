6034f07d28d0ce0fd16443c57bc654499b69db5b
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-08-30 19:24:15 +0000

    Release 0.10.2

--- trufflepig/bchain/posts.py
+++ trufflepig/bchain/posts.py
@@ -15,4 +15,6 @@ BODY_PREFIX = ''  # to announce tests etc.
 
 def truffle_comment(reward, votes, rank, topN_link, truffle_link, truffle_image_small=TRUFFLE_IMAGE_SMALL):
     """Creates a comment made under an upvoted toplist post"""
-    post = """**Congratulations!** Your post has been selected as a daily Steemit truffle! It is list
+    post = """**Congratulations!** Your post has been selected as a daily Steemit truffle! It is listed on **rank {rank}** of all contributions awarded today. You can find the [TOP DAILY TRUFFLE PICKS HERE.]({topN_link}) 
+    
+I upvoted your contribution because to my mind your post is at least **{reward} SBD** worth and should receive **{votes} votes**. It's now up to the lovely Steemit community to make this come
