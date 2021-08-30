ae64fffe08cb57b87a5e6df52f2c62e831373e60
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-08-30 14:39:30 +0000

    continue refactor

--- trufflepig/bchain/posts.py
+++ trufflepig/bchain/posts.py
@@ -4,4 +4,8 @@ import trufflepig.filters.textfilters as tftf
 
 
 TRUFFLE_IMAGE_SMALL = '![trufflepig](https://raw.githubusercontent.com/SmokinCaterpillar/TrufflePig/master/img/trufflepig17_small.png)'
-TRUFFLE_IMAGE = '![trufflepig](https://raw.githubusercontent.com/SmokinCaterpillar/Truffle
+TRUFFLE_IMAGE = '![trufflepig](https://raw.githubusercontent.com/SmokinCaterpillar/TrufflePig/master/img/trufflepig17.png)'
+DELEGATION_LINK = 'https://v2.steemconnect.com/sign/delegateVestingShares?delegator=&delegatee=trufflepig&vesting_shares={shares}%20VESTS'
+QUOTE_MAX_LENGTH = 496
+TAGS = ['steemit', 'curation', 'minnowsupport', 'technology', 'community']
+TRENDING_TAGS = ['steemit', 'curation', 'bots', 'technology', 'comm
