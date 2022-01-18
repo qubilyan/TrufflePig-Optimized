b4d156303c4065edd6192674342cc69526b0245b
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-01-18 20:46:19 +0000

    new edits

--- trufflepig/bchain/posts.py
+++ trufflepig/bchain/posts.py
@@ -524,4 +524,8 @@ By upvoting and resteeming this top list, you help covering the server costs and
 
 ## Delegate and Invest in the Bot
 
-If you feel generous, you can delegate Steem Power to me and boost my daily upvotes on the truffle posts in my other top list. In return, I will provide you with a *small* compensation for your trust in me and your locked Steem Power. **Half of my daily SBD and STEEM income will be paid out to all my delegators** propo
+If you feel generous, you can delegate Steem Power to me and boost my daily upvotes on the truffle posts in my other top list. In return, I will provide you with a *small* compensation for your trust in me and your locked Steem Power. **Half of my daily SBD and STEEM income will be paid out to all my delegators** proportional to their Steem Power share. Payouts will start 3 days after your delegation.
+
+Click on one of the following links to delegate **[2]({sp2}), [5]({sp5}), [10]({sp10}), [20]({sp20}), [50]({sp50}), [100]({sp100}), [200]({sp200}), [500]({sp500}), [1000]({sp1000}), [2000]({sp2000}),** or even **[5000 Steem Power]({sp5000})**. Thank You!
+
+Ch
