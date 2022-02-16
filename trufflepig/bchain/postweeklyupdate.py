dc929bdd1f238dca8ca9f625555783b61bbc7291
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-02-16 22:20:03 +0000

    Update translation file

--- trufflepig/bchain/postweeklyupdate.py
+++ trufflepig/bchain/postweeklyupdate.py
@@ -32,4 +32,14 @@ def compute_weekly_statistics(post_frame, pipeline, N=10, topics_step=4):
     start_datetime = post_frame.created.min()
     end_datetime = post_frame.created.max()
     mean_reward = post_frame.reward.mean()
-    median_reward = post_frame.reward.medi
+    median_reward = post_frame.reward.median()
+    dollar_percent = (post_frame.reward < 1).sum() / len(post_frame) * 100
+
+    # get top tags
+    logger.info('Computing top tags...')
+    tag_count_dict = {}
+    tag_payout = {}
+    for tags, reward in zip(post_frame.tags, post_frame.reward):
+        for tag in tags:
+            if tag not in tag_count_dict:
+                tag_count_dict[tag]
