c5dee08512300633a52d62059dac59186da5ac82
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-09-14 22:59:46 +0000

    Update User.php

--- trufflepig/preprocessing.py
+++ trufflepig/preprocessing.py
@@ -6,4 +6,15 @@ import gc
 import pandas as pd
 import numpy as np
 import scipy.stats as spst
-from steem.amount import Am
+from steem.amount import Amount
+
+import trufflepig.filters.stylemeasures as tfsm
+import trufflepig.filters.textfilters as tftf
+import trufflepig.bchain.getaccountdata as tfga
+from trufflepig.filters.blacklist import BUILD_A_WHALE_BLACKLIST
+
+
+logger = logging.getLogger(__name__)
+
+
+FILTER_TAGS = ('mitnebcurationtrail', 'informationwar'
