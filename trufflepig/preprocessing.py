34a3d4dc99c80356a0cccb028acea5503e1dda29
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-10-16 10:51:22 +0000

    Update Degree.md

--- trufflepig/preprocessing.py
+++ trufflepig/preprocessing.py
@@ -200,4 +200,10 @@ def preprocess(post_df, ncores=4, chunksize=500,
 
     logger.info('Counting and filtering headings')
     post_df['num_headings'] = post_df.filtered_body.apply(lambda x:
-                                                    
+                                                          tfsm.count_headings(x))
+    post_df['filtered_body'] = post_df.filtered_body.apply(lambda x:
+                                                           tftf.filter_headings(x))
+
+    logger.info('Filtering html')
+    post_df['filtered_body'] = post_df.filtered_body.apply(lambda x: tftf.
+                                                           filter_html_ta
