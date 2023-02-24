78d0df36a10ba1e2529fb0dc889b371277ea16b5
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-02-24 17:02:53 +0000

    - minor fixes

--- trufflepig/test_top_listmain.py
+++ trufflepig/test_top_listmain.py
@@ -10,4 +10,12 @@ import pandas as pd
 import trufflepig.bchain.getdata as tpgd
 import trufflepig.bchain.postdata as tppd
 import trufflepig.model as tpmo
-import trufflepig.preproces
+import trufflepig.preprocessing as tppp
+import trufflepig.utils as tfut
+import trufflepig.pigonduty as tfod
+import trufflepig.bchain.paydelegates as tpde
+import trufflepig.bchain.getaccountdata as tpad
+from trufflepig import config
+from trufflepig.utils import configure_logging
+import trufflepig.bchain.postweeklyupdate as tppw
+from trufflepig.bchain.mpsteem import
