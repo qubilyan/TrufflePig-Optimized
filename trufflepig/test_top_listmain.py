da8fa42ff53cc36743dfadb53e408850de9da019
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-02-24 18:17:35 +0000

    namespace changed

--- trufflepig/test_top_listmain.py
+++ trufflepig/test_top_listmain.py
@@ -18,4 +18,19 @@ import trufflepig.bchain.getaccountdata as tpad
 from trufflepig import config
 from trufflepig.utils import configure_logging
 import trufflepig.bchain.postweeklyupdate as tppw
-from trufflepig.bchain.mpsteem import
+from trufflepig.bchain.mpsteem import MPSteem
+from trufflepig.bchain.poster import Poster
+import trufflepig.trending0bidbots as tt0b
+
+
+logger = logging.getLogger(__name__)
+
+
+MAX_DOCUMENTS = 123000
+
+
+def parse_args():
+    """Parses command line arguments"""
+    parser = argparse.ArgumentParser(description='TrufflePig Bot')
+    parser.add_argument('--broadcast', action="store_false",
+       
