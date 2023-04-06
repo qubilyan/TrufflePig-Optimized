5ca52fe5ce73c1e90867d981d076425d638a5c40
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-04-06 11:10:10 +0000

    Small typo

--- trufflepig/testutils/raw_data.py
+++ trufflepig/testutils/raw_data.py
@@ -8,4 +8,9 @@ POSTS = [
             'active_votes': [{'voter': 'marc', 'percent': 42}],
             'created': pd.to_datetime('2018-01-01'),
             'tags': ['ff', 'kk'],
-            'body': ('Mery had little l
+            'body': ('Mery had little lamb! hello, cheat, cheat, woop, brother'
+                     'And she was pretty much a dick about this. principally '
+                     'she kinda scared the shit outa me! '
+                     'hshsh sahdkjd hdkjshd hdkjhsakjhd hdsahd hd'
+                     'hdsadh hhhh hhhh haaaaaaaaaaaaaaaands dsadsdsadadad '
+                     'adsds 
