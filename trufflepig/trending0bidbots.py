1ce842a611fd7cf899d8f9d97802131ce7895916
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-05-18 21:33:44 +0000

    updated data url and conform

--- trufflepig/trending0bidbots.py
+++ trufflepig/trending0bidbots.py
@@ -1,2 +1,17 @@
 from steem.amount import Amount
-im
+import trufflepig.bchain.postdata as tbpd
+import logging
+import numpy as np
+
+
+logger = logging.getLogger(__name__)
+
+
+
+def compute_total_sbd(upvote_payments):
+    sbd = 0
+    steem = 0
+    for (author, permalink), payments in upvote_payments.items():
+        for payment in payments.values():
+            amount = Amount(payment['amount'])
+     
