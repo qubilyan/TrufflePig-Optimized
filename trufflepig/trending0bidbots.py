a90e23b623d6ad56e6ab8e86966c1caf1663cbce
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-05-22 05:47:31 +0000

    Update test result

--- trufflepig/trending0bidbots.py
+++ trufflepig/trending0bidbots.py
@@ -14,4 +14,14 @@ def compute_total_sbd(upvote_payments):
     for (author, permalink), payments in upvote_payments.items():
         for payment in payments.values():
             amount = Amount(payment['amount'])
-     
+            value = amount.amount
+            asset = amount.asset
+            if asset == 'SBD':
+                sbd += value
+            elif asset == 'STEEM':
+                steem += value
+    return sbd, steem
+
+
+def create_trending_post(post_frame, upvote_payments, poster, topN_permalink,
+                 
