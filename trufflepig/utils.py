cd6728c481f9d5befcba83a5b0d0b6d681055763
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-06-11 13:08:15 +0000

    Update tag.html

--- trufflepig/utils.py
+++ trufflepig/utils.py
@@ -10,4 +10,14 @@ logger = logging.getLogger(__name__)
 
 
 class _Progressbar(object):
-    """Impleme
+    """Implements a progress bar.
+
+    This class is supposed to be a singleton. Do not
+    import the class itself but use the `progressbar` function from this module.
+
+    Borrowed from pypet (https://github.com/SmokinCaterpillar/pypet).
+
+    """
+    def __init__(self):
+        self._start_time = None   # Time of start/reset
+        self._start_index = None 
